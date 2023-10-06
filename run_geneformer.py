d2d1b3e75c43b77246cc45e0e1a82b7a322b781e
LiSu935 <57879262+LiSu935@users.noreply.github.com>
2023-10-06 11:29:22 -0500

    Update run_geneformer.py

--- run_geneformer.py
+++ run_geneformer.py
@@ -2,18 +2,20 @@ from geneformer import TranscriptomeTokenizer
 
 # if previously wrote the 'ensembl_id', the following might not be necessary.
 import scanpy as sc 
-tem = sc.read_h5ad("/mnt/pixstor/dbllab/suli/Alg_development/use_scgpt/NSCLC_subsetted_raw.h5ad")
-tem.var['ensembl_id'] = tem.var['features']
-tem.__dict__['_raw'].__dict__['_var'] = tem.__dict__['_raw'].__dict__['_var'].rename(columns={'_index': 'ensembl_id'})
-tem.write_h5ad("/mnt/pixstor/dbllab/suli/Alg_development/use_scgpt/NSCLC_subsetted_raw.h5ad")
+#tem = sc.read_h5ad("/mnt/pixstor/dbllab/suli/Alg_development/use_geneformer/data/NSCLC_subsetted/NSCLC_subsetted_raw.h5ad")
+#tem.var['ensembl_id'] = tem.var['features']
+#tem.__dict__['_raw'].__dict__['_var'] = tem.__dict__['_raw'].__dict__['_var'].rename(columns={'_index': 'ensembl_id'})
+#tem.write_h5ad("/mnt/pixstor/dbllab/suli/Alg_development/use_geneformer/data/NSCLC_subsetted/NSCLC_subsetted_raw.h5ad")
 
+input_dir = "/mnt/pixstor/dbllab/suli/Alg_development/use_geneformer/data/NSCLC_subsetted/"
+output_dir = input_dir
+prefix = "NSCLC_subsetted"
 
-tk = TranscriptomeTokenizer({"cell_type": "cell_type"}, nproc=2)
+tk = TranscriptomeTokenizer(nproc=15)
 
-
-tk.tokenize_data("/mnt/pixstor/dbllab/suli/Alg_development/use_scgpt/", 
-                 "/mnt/pixstor/dbllab/suli/Alg_development/use_scgpt/", 
-                 "NSCLC_subsetted", 
+tk.tokenize_data(input_dir, 
+                 output_dir, 
+                 prefix, 
                  file_format="h5ad")
 
 
@@ -33,9 +35,9 @@ embex = EmbExtractor(model_type="Pretrained",
 # extracts embedding from input data
 # example dataset: https://huggingface.co/datasets/ctheodoris/Genecorpus-30M/tree/main/example_input_files/cell_classification/disease_classification/human_dcm_hcm_nf.dataset
 embs = embex.extract_embs("/mnt/pixstor/dbllab/suli/tools_related/geneformer/geneformer-12L-30M",
-                          "/mnt/pixstor/dbllab/suli/Alg_development/use_geneformer/data/NSCLC_subsetted/NSCLC_subsetted.dataset/",
-                          "/mnt/pixstor/dbllab/suli/Alg_development/use_geneformer/data/NSCLC_subsetted/",
-                          "NSCLC_subsetted_geneformer_out")
+                          input_dir+prefix+".dataset/",
+                          output_dir,
+                          prefix+"_geneformer_out")
 
 
 #File "/mnt/pixstor/data/lsxgf/miniconda/envs/py-geneformer/lib/python3.8/site-packages/huggingface_hub/utils/_validators.py", line 164, in validate_repo_id
