1df06ad9a0634d2bb5ff9ae26e89c57bcd65286a
LiSu935 <57879262+LiSu935@users.noreply.github.com>
2023-10-11 12:40:52 -0500

    Update run_geneformer.py

--- run_geneformer.py
+++ run_geneformer.py
@@ -7,9 +7,9 @@ import scanpy as sc
 #tem.__dict__['_raw'].__dict__['_var'] = tem.__dict__['_raw'].__dict__['_var'].rename(columns={'_index': 'ensembl_id'})
 #tem.write_h5ad("/mnt/pixstor/dbllab/suli/Alg_development/use_geneformer/data/NSCLC_subsetted/NSCLC_subsetted_raw.h5ad")
 
-input_dir = "/mnt/pixstor/dbllab/suli/Alg_development/use_geneformer/data/NSCLC_subsetted/"
-output_dir = input_dir
-prefix = "NSCLC_subsetted"
+#input_dir = "/mnt/pixstor/dbllab/suli/Alg_development/use_geneformer/data/NSCLC_subsetted/"
+#output_dir = input_dir
+#prefix = "NSCLC_subsetted"
 
 
 
@@ -65,7 +65,7 @@ def plot_umap(embs_df, emb_dims, label, output_file, kwargs_dict):
     only_embs_df.columns = pd.RangeIndex(0, only_embs_df.shape[1], name=None).astype(str)
     vars_dict = {"embs": only_embs_df.columns}
     obs_dict = {"cell_id": list(only_embs_df.index),
-                f"{label}": label}
+                "cell_type": label}
     adata = anndata.AnnData(X=only_embs_df, obs=obs_dict, var=vars_dict)
     sc.tl.pca(adata, svd_solver='arpack')
     sc.pp.neighbors(adata)
@@ -74,17 +74,16 @@ def plot_umap(embs_df, emb_dims, label, output_file, kwargs_dict):
     sns.set_style("white")
     default_kwargs_dict = {"palette":"Set2", "size":200}
     if kwargs_dict is not None:
-        default_kwargs_dict.update(kwargs_dict)
-        
-    sc.pl.umap(adata, color=label, save=output_file, **default_kwargs_dict)
+      default_kwargs_dict.update(kwargs_dict)
+    sc.pl.umap(adata, color="cell_type", save=output_file, **default_kwargs_dict)
 
 
 import pandas as pd
 import scanpy as sc 
 
-label = "celltype"
+label = "cell_type"
 prefix = "NSCLC_subsetted"
-output_directory = "/mnt/pixstor/dbllab/suli/Alg_development/use_geneformer/data/NSCLC_subsetted/"
+output_directory = output_dir
 output_prefix = prefix+"_geneformer_out"
 output_prefix_label = "_" + output_prefix + f"_umap_{label}"
 output_file = (Path(output_directory) / output_prefix_label).with_suffix(".pdf")
@@ -93,21 +92,4 @@ embs = pd.read_csv("/mnt/pixstor/dbllab/suli/Alg_development/use_geneformer/data
 
 plot_umap(embs_df=embs, emb_dims=embs.shape[1], label=label_list, output_file=output_file, kwargs_dict=None)
 
-only_embs_df = embs_df.iloc[:,:emb_dims]
-only_embs_df.index = pd.RangeIndex(0, only_embs_df.shape[0], name=None).astype(str)
-only_embs_df.columns = pd.RangeIndex(0, only_embs_df.shape[1], name=None).astype(str)
-vars_dict = {"embs": only_embs_df.columns}
-obs_dict = {"cell_id": list(only_embs_df.index),
-            "celltype": label}
-adata = anndata.AnnData(X=only_embs_df, obs=obs_dict, var=vars_dict)
-sc.tl.pca(adata, svd_solver='arpack')
-sc.pp.neighbors(adata)
-sc.tl.umap(adata)
-sns.set(rc={'figure.figsize':(10,10)}, font_scale=2.3)
-sns.set_style("white")
-default_kwargs_dict = {"palette":"Set2", "size":200}
-if kwargs_dict is not None:
-  default_kwargs_dict.update(kwargs_dict)
-  
-sc.pl.umap(adata, color="celltype", save=output_file, **default_kwargs_dict)
 
