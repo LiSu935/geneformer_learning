c0474503a8c6c4db2f54c54ac07c1af2ea5f5b3c
LiSu935 <57879262+LiSu935@users.noreply.github.com>
2023-11-05 01:02:51 -0500

    Update symbol_list_2_IDmapping_table.R

--- symbol_list_2_IDmapping_table.R
+++ symbol_list_2_IDmapping_table.R
@@ -1,9 +1,26 @@
+# on HB, conda activate r-singlecell-r4.1-py3.10
+# to use: 
+# SCRIPT='symbol_list_2_IDmapping_table.R'
+# Rscript ${SCRIPT} -i PATH_OF_input_dir
+
 library(Seurat)
 library(SeuratDisk)
 library(tools)
 library("AnnotationDbi")
 library("org.Hs.eg.db")
 library(dplyr)
+
+library(optparse)
+option_list <- list(
+  make_option(c("-i", "--input_dir"), type = "character", default = "/group/xulab/Su_Li/spaVG/dataset/Simulation_20220110/Scenario_1/", 
+              help = "input path", metavar = "character")
+ 
+)
+opt_parser = OptionParser(option_list = option_list)
+opt = parse_args(opt_parser)
+
+# input parameter
+input_dir <- opt$input_dir
 
 gen_id_mapping_df = function(ORI_INPUT_DIR){
   for (gene_name_File_INPUT in list.files(path = ORI_INPUT_DIR, pattern = "genesymbol_list\\.txt$")) {
@@ -34,6 +51,6 @@ gen_id_mapping_df = function(ORI_INPUT_DIR){
   }
 }
 
-ORI_INPUT_DIR = "/mnt/pixstor/dbllab/suli/Alg_development/use_geneformer/data/Benchmark_data_top2000/COVID/"
-gen_id_mapping_df(ORI_INPUT_DIR)
+#ORI_INPUT_DIR = "/mnt/pixstor/dbllab/suli/Alg_development/use_geneformer/data/Benchmark_data_top2000/COVID/"
+gen_id_mapping_df(ORI_INPUT_DIR=input_dir)
 
